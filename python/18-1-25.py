2dbf0c31548d5e6829d47fecf56ed2d0d90a193a
Mohammed Afreed Pasha <afreed0076@gmail.com>
2025-01-18 03:52:51 +0000

    pc1

---
+++ python/18-1-25.py
@@ -0,0 +1,8 @@
+                               str=input("Enter the string ")
+rev=str[::-1]
+print(rev)
+
+if(str== rev):
+        print(str," is a palindrom")
+else:
+        print(str," is not a palindrom")
