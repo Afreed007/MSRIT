0bfc5af2682243c49d0bdaf036874582f8e3f91c
Mohammed Afreed Pasha <afreed0076@gmail.com>
2025-01-20 14:29:20 +0530

    Update 4E.py

--- python/4E.py
+++ python/4E.py
@@ -9,3 +9,18 @@ for i in s:
 print(f"biggest word in line is {b}")
 print(f"Lenght of word is {l}")
 
+
+line=input("enter the string:")
+s= line.split(" ")
+bword=""
+bwordlist=[]
+for i in s:
+        if(len(i)>len(bword)):
+                bword=i
+                bwordlist=[bword]
+        elif(len(i)==len(bword)):
+                bword=i
+                bwordlist+=[bword]     
+print(f"biggest words in line are {bwordlist}")
+print(f"Lenght of word is {len(bword)}")
+
