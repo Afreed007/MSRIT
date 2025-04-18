ba0adb34834d4757aaf1734ce4e6c4425c5c1d9d
afreed007 <afreed0076@gmail.com>
2025-04-18 23:16:28 +0530

    sem end

--- python/4D.py
+++
@@ -1,8 +0,0 @@
-str=input("enter the string:")
-char=input("Enter the string:")
-count=0
-for i in range(0,len(str)):
-        if(str[i]==char):
-                count+=1
-                print("occured at index", i)
-print("count=",count)
